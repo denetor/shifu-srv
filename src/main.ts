75e8825fd7314b54b4bb16dc79878a551937c734
Nicola Da Rold <nicola.darold@gmail.com>
2020-03-22 14:22:07 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger configuration
+  const swaggerOptions = new DocumentBuilder()
+      .setTitle('Shifu API')
+      .setDescription('The taolu API description')
+      .setVersion('1.0')
+      .addTag('taolu')
+      .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
