26c78d0eb6a044f5c0faabd7a559cc5c8f01369d
Nicola Da Rold <nicola.darold@gmail.com>
2020-03-24 23:35:43 +0100

    aggiunta generazione JWT alla login

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from "./auth/local-auth.guard";
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+      private readonly appService: AppService,
+      private authService: AuthService
+  ) {}
 
   @Get()
   getHello(): string {
@@ -16,7 +19,7 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 
 }
